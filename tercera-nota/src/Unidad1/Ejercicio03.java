aec10acdc8cbaf54b394033bc55bb07ec2b2c681
sebz <jscabarcan@ufpso.edu.co>
2026-09-04 20:37:42 -0500

    primeros ejercicios

--- tercera-nota/src/Unidad1/Ejercicio03.java
+++ tercera-nota/src/Unidad1/Ejercicio03.java
@@ -1,7 +1,20 @@
-package Unidad1;
+
+import java.util.Scanner;
 
 public class Ejercicio03 {
     public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.println("ingrese su nombre");
+        String nombre = leer.nextLine();
+
+        System.out.println("ingrese su edad");
+         int edad = leer.nextInt();
+
+        System.out.println("ingrese su altura");
+        double altura = leer.nextDouble();
+
+        System.out.println("Hola amigo, tu nombre es" + nombre + ". Tienes " + edad + " años y tu altura es de " + altura);
         
     }
     
